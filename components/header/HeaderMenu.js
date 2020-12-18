f4110770cb844eeb86d0e464a4394caa6b33a4ad
IvanLeonidovich <pavel.c@solid-sl.com>
2020-12-18 21:47:10 +0200

    add key to map func Header Components

--- components/header/HeaderMenu.js
+++ components/header/HeaderMenu.js
@@ -4,20 +4,27 @@ import {MainLayout} from "../MainLayout";
 import {useRouter} from "next/router";
 
 export function HeaderMenu({nav}) {
-    var links = Object.values(nav)
+    let links = [];
+
+    for (const [key, value] of Object.entries(nav)) {
+        if (key) {
+            links[key] = value;
+        }
+    }
+
     return (
         <>
             <ul className="navbar-nav menu pl_120 mr-auto ml-auto">
                 {
-                    links.map((link,index) =>
+                    links.map((link, index) =>
                         (
-                            <li key={link} className="nav-item active">
-                                <Link href={link.url}>
-                                    <a className="nav-link dropdown-toggle">
-                                        {link.title}
-                                    </a>
-                                </Link>
-                            </li>
+                             <li key={index} className="nav-item active">
+                                 <Link href={link.url}>
+                                     <a className="nav-link dropdown-toggle">
+                                         {link.title}
+                                     </a>
+                                 </Link>
+                             </li>
                         )
                     )
                 }
